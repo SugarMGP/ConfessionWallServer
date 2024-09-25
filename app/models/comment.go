c104ff115143a41be2bd3522349215cbad3bff15
Lufer-210 <591567374@qq.com>
2024-09-25 22:46:30 +0800

    Add files via upload

--- app/models/comment.go
+++ app/models/comment.go
@@ -1,9 +1,14 @@
 package models
 
+// Comment 评论模型
+
 import "time"
 
 type Comment struct {
-	Username  string    `json:"-"`
-	Comment   string    `json:"comment"`
-	CreatedAt time.Time `json:"-"`
+	ID        int       `gorm:"primaryKey;autoIncrement"`
+	PostID    int       `gorm:"not null"`
+	UserID    int       `gorm:"not null"`
+	Content   string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"not null"`
+	UpdatedAt time.Time `gorm:"not null"`
 }
