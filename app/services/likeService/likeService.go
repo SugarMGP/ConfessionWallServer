30a7acc37b1beb48a7b9b9716e36630dc87f4a62
SugarMGP <2350745751@qq.com>
2024-10-03 20:03:12 +0800

    fix: 修复点赞相关功能

--- app/services/likeService/likeService.go
+++ app/services/likeService/likeService.go
@@ -21,27 +21,27 @@ func GetCommentKey(CommentID uint) string {
 func PostLikeCount(PostID uint) error {
 	var keys string = GetPostKey(PostID)
 	redisClient := rds.GetRedis()
-	defer redisClient.Close()
 
 	postCount := redis.BitCount{Start: 0, End: -1}
-	err := redisClient.BitCount(context.Background(), keys, &postCount).Err()
+	count, err := redisClient.BitCount(context.Background(), keys, &postCount).Result()
 	if err != nil {
 		return err
 	}
-	result := database.DB.Where("id = ?", PostID).First(&models.Post{}).Update("likes", postCount)
+
+	result := database.DB.Where("id = ?", PostID).First(&models.Post{}).Update("likes", count)
 	return result.Error
 }
 
 func CommentLikeCount(CommentID uint) error {
 	var keys string = GetCommentKey(CommentID)
 	redisClient := rds.GetRedis()
-	defer redisClient.Close()
 
 	commentCount := redis.BitCount{Start: 0, End: -1}
-	err := redisClient.BitCount(context.Background(), keys, &commentCount).Err()
+	count, err := redisClient.BitCount(context.Background(), keys, &commentCount).Result()
 	if err != nil {
 		return err
 	}
-	result := database.DB.Where("id = ?", CommentID).First(&models.Comment{}).Update("likes", commentCount)
+
+	result := database.DB.Where("id = ?", CommentID).First(&models.Comment{}).Update("likes", count)
 	return result.Error
 }
